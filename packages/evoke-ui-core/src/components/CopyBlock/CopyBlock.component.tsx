541c31b5b08b533a8ddb2645e83f8c278951b7d3
Adam Colpitts <adam@colpitts.dev>
2022-07-24 16:25:17 -0300

    feat: add textSize prop to CopyBlock

--- packages/evoke-ui-core/src/components/CopyBlock/CopyBlock.component.tsx
+++ packages/evoke-ui-core/src/components/CopyBlock/CopyBlock.component.tsx
@@ -1,22 +1,33 @@
-import React, { FC, HTMLAttributes, ReactNode } from 'react';
-import { Heading } from '../Heading';
-import { Text } from '../Text';
+import React, { FC, HTMLAttributes, ReactNode } from 'react'
+import { Heading } from '../Heading'
+import { Text } from '../Text'
 
-import "./CopyBlock.styles.css";
+import './CopyBlock.styles.css'
 
 export interface CopyBlockProps extends HTMLAttributes<HTMLDivElement> {
-  heading: string,
+  headingLevel?: 1 | 2 | 3
+  textSize?: 'sm' | 'base' | 'lg'
+  title: string
   content: string
 }
 
 /**
  * CopyBlock component
  */
-export const CopyBlock: FC<CopyBlockProps> = ({heading, content, ...props}) => {
+export const CopyBlock: FC<CopyBlockProps> = ({
+  title,
+  content,
+  headingLevel = 3,
+  textSize = 'base',
+  ...props
+}) => {
   return (
     <div className="CopyBlock" {...props}>
-      <Heading level="h3" className="CopyBlock__Heading">{heading}</Heading><br/>
-      <Text className="CopyBlock__Text">{content}</Text>
+      <Heading level={`h${headingLevel}`} className="CopyBlock__Heading">
+        {title}
+      </Heading>
+      <br />
+      <Text className="CopyBlock__Text" size={textSize}>{content}</Text>
     </div>
-  );
-};
+  )
+}
--- packages/evoke-ui-core/src/components/Text/Text.component.tsx
+++ packages/evoke-ui-core/src/components/Text/Text.component.tsx
@@ -1,22 +1,23 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react'
+import React, { FC, HTMLAttributes, ReactChild } from "react";
 
 export interface TextProps extends HTMLAttributes<HTMLParagraphElement> {
-  children?: ReactChild
-  className?: string
+  children?: ReactChild;
+  className?: string;
+  size?: "sm" | "base" | "lg";
 }
 
 /**
  * Text component
  */
-export const Text: FC<TextProps> = ({ className, children, ...props }) => {
+export const Text: FC<TextProps> = ({ size="base",className, children, ...props }) => {
   return (
     <p
-      className={`font-secondary text-dark dark:text-white ${
-        className ? className : ''
+      className={`font-secondary text-dark dark:text-white ${size !== "base" ? `text-${size}` : ""} ${
+        className ? className : ""
       }`}
       {...props}
     >
       {children}
     </p>
-  )
-}
+  );
+};
