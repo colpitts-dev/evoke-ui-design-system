bc48bad668bbeb4ee448989b6cc566496d77c1f4
acolpitts <adam@colpitts.dev>
2022-11-02 22:20:48 -0300

    feat: update button props to accept click handler

--- packages/evoke-ui-core/src/components/Button/Button.component.tsx
+++ packages/evoke-ui-core/src/components/Button/Button.component.tsx
@@ -1,37 +1,41 @@
-import * as React from "react";
+import { FC, HTMLAttributes } from 'react'
 
 type ButtonTheme =
-  | "primary"
-  | "secondary"
-  | "success"
-  | "info"
-  | "warn"
-  | "danger";
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'info'
+  | 'warn'
+  | 'danger'
 
-export interface ButtonProps {
-  children: React.ReactNode;
-  theme: ButtonTheme;
+export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode
+  theme: ButtonTheme
 }
 
-export function Button(props: ButtonProps) {
-  let foreground: string;
-  switch (props.theme) {
-    case "primary":
-    case "secondary":
-    case "danger":
-      foreground = "white";
-      break;
+export const Button: FC<ButtonProps> = ({theme, onClick, ...props}) => {
+  let foreground: string
+  switch (theme) {
+    case 'primary':
+    case 'secondary':
+    case 'danger':
+      foreground = 'white'
+      break
     default:
-      foreground = "black";
+      foreground = 'black'
   }
   return (
-    <button className={`py-3 text-${foreground} px-7 bg-${props.theme}`}>
+    <button
+      className={`py-3 text-${foreground} px-7 bg-${theme}`}
+      onClick={onClick}
+      {...props}
+    >
       {props.children}
     </button>
-  );
+  )
 }
 
-Button.displayName = "Button";
+Button.displayName = 'Button'
 Button.defaultProps = {
-  theme: "primary",
-};
+  theme: 'primary',
+}
